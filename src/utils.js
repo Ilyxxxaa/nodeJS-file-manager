5bdc10a93e9bc26bac3df8db06df34ac40b8e042
Ilya Kotsur <99085805+Ilyxxxaa@users.noreply.github.com>
2024-02-06 01:40:43 +0300

    feat: add opport to enter file/folders consist of 2 and more words

--- src/utils.js
+++ src/utils.js
@@ -15,7 +15,16 @@ export const sayGoodbye = (username) => {
 };
 
 export const parseInput = (input) => {
-  return input.split(" ");
+  let args = input.split(" ");
+  const quoteRegExp = /"|'/g;
+  if (quoteRegExp.test(args)) {
+    const quotesRegExp = /["'] | ["']/;
+    args = args
+      .join(" ")
+      .split(quotesRegExp)
+      .map((arg) => arg.replace(quoteRegExp, ""));
+  }
+  return args;
 };
 
 export const validateCommand = (command, args) => {
